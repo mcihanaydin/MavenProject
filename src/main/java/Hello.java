726ae2d8d211c8f421f5ce6fe6e4bd5d0b73a15f
Cihan <mcihanaydin@gmail.com>
2022-04-27 14:56:27 +0300

    Update added hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("deasdsd");
+        System.out.println("Doing more woek");
     }
 }
